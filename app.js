5f36a75ce46cfd9e283ea5d1aa4d4f39e0ad7e62
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-04-26 21:44:09 +0530

    readmore,delete,edit feat added

--- app.js
+++ app.js
@@ -9,29 +9,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    var formdets = []
-    fs.readdir("./files",function(err,files){
-        files.forEach(files => {
-           var filedesc = fs.readFileSync(`./files/${files}`,"utf-8",function(err,files){
-        })
-        var fn = files
-        formdets.push({head:fn,desc:filedesc})
-    })
-        res.render("index",{formdets});
-
-    })
+  var formdets = [];
+  fs.readdir("./files", function (err, files) {
+    files.forEach((files) => {
+      var filedesc = fs.readFileSync(
+        `./files/${files}`,
+        "utf-8",
+        function (err, files) {}
+      );
+      formdets.push({ head: files, desc: filedesc });
+    });
+    res.render("index", { formdets });
+  });
 });
 
-app.post('/create', (req, res) => {
-    const title = req.body.head.split(' ').join('') + '.txt';
-        fs.writeFile(`./files/${title}`, req.body.desc, (err, data) => {
-            if(err) return err
-            else res.redirect('/')
-        })
-
+app.post("/create", (req, res) => {
+  const title = req.body.head.split(" ").join("") + ".txt";
+  fs.writeFile(`./files/${title}`, req.body.desc, (err) => {
+    if (err) return err;
+    else res.redirect("/");
+  });
+});
+app.get("/read/:filehead", (req, res) => {
+  fs.readFile(`./files/${req.params.filehead}`, "utf-8", function (err, files) {
+    res.render("read", { head: req.params.filehead, desc: files });
+  });
+});
+app.get("/update/:filehead", (req, res) => {
+  fs.readFile(`./files/${req.params.filehead}`, "utf-8", function (err, files) {
+    res.render("edit", { head: req.params.filehead, desc: files });
+  });
 });
 
-const port = 3100;
+app.post("/update/:filehead", (req, res) => {
+      fs.writeFile(`./files/${req.params.filehead}`, req.body.desc, (err) => {
+        if (err) return err;
+      });
+       res.redirect("/");
+    });
+
+app.get("/delete/:filename", (req, res) => {
+  fs.unlink(`./files/${req.params.filename}`, (err) => {
+    res.redirect("/");
+  });
+});
+
+const port = 3200;
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
